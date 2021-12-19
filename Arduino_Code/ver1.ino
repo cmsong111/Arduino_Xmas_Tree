52365ed23d95d2da11de616c9b24977fa47802c2
Namju Kim <cmsong111@naver.com>
2021-12-19 18:20:11 +0900

    Update ver1.ino

--- Arduino_Code/ver1.ino
+++ Arduino_Code/ver1.ino
@@ -29,6 +29,25 @@ void slowy()
   }
 }
 
+void very_fast()
+{
+  int off = 0;
+  int on = 255;
+  analogWrite(9,on);
+  analogWrite(10,off);
+  analogWrite(11,on);
+  analogWrite(12,off);
+  analogWrite(13,on);
+  delay(100);
+  analogWrite(9,off);
+  analogWrite(10,on);
+  analogWrite(11,off);
+  analogWrite(12,on);
+  analogWrite(13,off);
+  delay(100);
+
+}
+
 
 //arduino setup
 void setup()
@@ -47,13 +66,24 @@ void setup()
 int randNumber;
 void loop()
 {
-  randNumber = random(1);
+  randNumber = random(2);
   switch (randNumber)
   {
   case 0:
-    slowy();
+    for(int i = 0; i<10;i++)
+    {
+      slowy();
+    }
+    
     break;
-  
+
+  case 1:
+  for(int i = 0; i<10;i++)
+    {
+      very_fast();
+    }
+    break;
+
   default:
     break;
   }
